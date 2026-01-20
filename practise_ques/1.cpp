0c9a17ddb09a2083a7740ad27d4b5fb538974b6a
Saubhagya Singh <stochastic911@gmail.com>
2026-01-20 18:14:12 +0530

    20 jan fix

--- practise_ques/1.cpp
+++ practise_ques/1.cpp
@@ -1,29 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long lli;
-#define endl '\n'
-#define tab '\t'
-void solve()
-{
-    int n;
-    cin>>n;int ct=1;
-    if(n&1)
-    cout<<-1<<endl;
-    
-    else 
-    {
-         for(int i=1;i<=n;i+=2)
-         {
-            cout<<ct<<" "<<ct<<" ";
-            ct+=1;
-         }
-        
-    }
-    cout<<endl;
-    return ;
-}
+
 signed main()
 {
-  cin.tie(0); cout.tie(0);
-  int t;cin>>t;while(t--)solve();
+  cout<<"Hello"<<endl;
 }
